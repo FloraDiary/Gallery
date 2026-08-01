5b8e35552dcf917b928a9be5906624b64139cd4f
FloraDiary <zbm@email.com>
2026-08-01 00:57:21 +0000

    works

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,8 @@
 /*Name this external file gallery.js*/
 
 function upDate(previewPic){
-    document.getElementById("image").innerHTML=(this)
+    document.getElementById("image").style.backgroundImage = `url('${previewPic.src}')`;
+    document.getElementById("image").innerHTML = previewPic.alt;
  /* In this function you should 
     1) change the url for the background image of the div with the id = "image" 
     to the source file of the preview image
@@ -12,7 +13,9 @@ function upDate(previewPic){
   
 	}
 
-	function unDo(){
+function unDo(){
+    document.getElementById("image").style.backgroundImage = `url('')`;
+    document.getElementById("image").innerHTML = "Hover over an image below to display here.";
      /* In this function you should 
     1) Update the url for the background image of the div with the id = "image" 
     back to the orginal-image.  You can use the css code to see what that original URL was
